25178595d13546154e2e38b6387bf49fcedfdc76
Rizky Jeremia Simanjutak <90759795+RizkySimanjuntak@users.noreply.github.com>
2023-03-19 23:18:02 +0700

    Delete Update Shoot directory

--- Update Shoot/script/InviniteBackGround.gd
+++
@@ -1,7 +0,0 @@
-extends ParallaxBackground
-
-
-export (float) var scrolling_speed = 200.0
-
-func _process(delta):
-	scroll_offset.x -= scrolling_speed * delta
--- Update Shoot/script/bullet.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-
-export var speed: float = 2000
-
-func _physics_process(delta):
-	position.x += speed * delta
